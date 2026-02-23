dcace7f4d7e516e416f79ee59e00d86053c0dad8
Ajith Arwind <arwindajith@gmail.com>
2026-02-23 23:27:06 +0530

    graded assignment 5 commited

---
+++ if_else_elif.py
@@ -0,0 +1,21 @@
+# odd or even using if else
+
+'''num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")'''
+
+
+# to find whether a number ends with 0 or 5 or any other number
+
+number = int(input("enter any number: "))
+if number % 10 == 0:
+    print(f"{number} ends with 0")
+elif number % 5 == 0:
+    print(f"{number} ends with 5")
+else:
+    print(f"{number} ends with other numbers")
+
+
+# Grade of a student based on marks
